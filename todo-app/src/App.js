e1bea728359c0167bce27873ea3c400e73f244d7
Anish Anand <anishanand3733@gmail.com>
2022-10-16 01:09:48 +0530

    Break down into components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
+
+import AddTaskForm from "./components/AddTaskForm";
+import UpdateForm from "./components/UpdateForm";
+import ToDo from "./components/ToDo";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleCheck,
-  faPen,
-  faTrashCan,
-} from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
@@ -76,95 +75,31 @@ function App() {
       <br />
 
       {updateData && updateData ? (
-        <>
-          <div className="row">
-            <div className="col">
-              <input
-                value={updateData && updateData.title}
-                onChange={(e) => changeTask(e)}
-                className="form-control form-control-lg"
-              />
-            </div>
-            <div className="col-auto">
-              <button
-                className="btn btn-lg btn-success mr-20"
-                onClick={updateTask}
-              >
-                Update
-              </button>
-              <button className="btn btn-lg btn-warning" onClick={cancelUpdate}>
-                Cancel
-              </button>
-            </div>
-          </div>
-          <br />
-        </>
+        <UpdateForm 
+        updateData={updateData}
+        changeTask={changeTask}
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}
+        />
       ) : (
-        <>
-          <div className="row">
-            <div className="col">
-              <input
-                value={newTask}
-                onChange={(e) => setNewTask(e.target.value)}
-                className="form-control form-control-lg"
-              />
-            </div>
-            <div className="col-auto">
-              <button className="btn btn-lg btn-success" onClick={addTask}>
-                Add Task
-              </button>
-            </div>
-          </div>
-          <br />
-        </>
+        <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+        />
       )}
 
       {/* Display Todos */}
 
       {toDo && toDo.length ? "" : "No Tasks!"}
 
-      {toDo &&
-        toDo
-          .sort((a, b) => (a.id > b.id ? 1 : -1))
-          .map((task, index) => {
-            return (
-              <React.Fragment key={task.id}>
-                <div className="col taskBg">
-                  <div className={task.status ? "done" : ""}>
-                    <span className="taskNumber">{index + 1}</span>
-                    <span className="taskText">{task.title}</span>
-                  </div>
-                  <div className="iconsWrap">
-                    <span
-                      onClick={(e) => markDone(task.id)}
-                      title="Completed / Not Completed"
-                    >
-                      <FontAwesomeIcon icon={faCircleCheck} />
-                    </span>
+      <ToDo 
+      toDo={toDo}
+      markDone={markDone}
+      setUpdateData={setUpdateData}
+      deleteTask={deleteTask}
+      />
 
-                    {task.status ? null : (
-                      <span
-                        title="Edit"
-                        onClick={() =>
-                          setUpdateData({
-                            id: task.id,
-                            title: task.title,
-                            status: task.status ? true : false,
-                          })
-                        }
-                      >
-                        <FontAwesomeIcon icon={faPen} />
-                      </span>
-                    )}
-
-                    <span onClick={() => deleteTask(task.id)} title="Delete">
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </span>
-                  </div>
-                </div>
-              </React.Fragment>
-            );
-          })}
     </div>
   );
 }
